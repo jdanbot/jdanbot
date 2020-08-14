e6c7781d575c39de1f08addfdb88105ae2ac0be6
jDan735 <daniel734@bk.ru>
2020-08-14 07:55:50 +0300

    Add text formating commands

--- bot.py
+++ bot.py
@@ -52,6 +52,77 @@ separator = "/" if os.name == "posix" or os.name == "macos" else "\\"
 
 #bot.delete_message(-1001176998310, 164427)
 
+@bot.message_handler(["title"])
+def title(message):
+    if len(message.text.split(maxsplit=1)) == 2:
+        text = message.text.split(maxsplit=1)[1]
+
+    elif hasattr(message.reply_to_message, "text"):
+        text = message.reply_to_message.text
+
+    else:
+        bot.reply_to(message, "Ответь на сообщение")
+        return
+
+    bot.reply_to(message, text.title())
+
+@bot.message_handler(["upper"])
+def upper(message):
+    if len(message.text.split(maxsplit=1)) == 2:
+        text = message.text.split(maxsplit=1)[1]
+
+    elif hasattr(message.reply_to_message, "text"):
+        text = message.reply_to_message.text
+
+    else:
+        bot.reply_to(message, "Ответь на сообщение")
+        return
+    bot.reply_to(message, text.upper())
+
+@bot.message_handler(["lower"])
+def lower(message):
+    if len(message.text.split(maxsplit=1)) == 2:
+        text = message.text.split(maxsplit=1)[1]
+
+    elif hasattr(message.reply_to_message, "text"):
+        text = message.reply_to_message.text
+
+    else:
+        bot.reply_to(message, "Ответь на сообщение")
+        return
+    bot.reply_to(message, text.lower())
+
+@bot.message_handler(["len"])
+def len_(message):
+    if len(message.text.split(maxsplit=1)) == 2:
+        text = message.text.split(maxsplit=1)[1]
+
+    elif hasattr(message.reply_to_message, "text"):
+        text = message.reply_to_message.text
+
+    else:
+        bot.reply_to(message, "Ответь на сообщение")
+        return
+    bot.reply_to(message, len(text))
+
+@bot.message_handler(["markdown"])
+def md(message):
+    if len(message.text.split(maxsplit=1)) == 2:
+        text = message.text.split(maxsplit=1)[1]
+
+    elif hasattr(message.reply_to_message, "text"):
+        text = message.reply_to_message.text
+
+    else:
+        bot.reply_to(message, "Ответь на сообщение")
+        return
+
+    try:
+        bot.reply_to(message, text, parse_mode="Markdown")
+        
+    except:
+        bot.reply_to(message, "Невалидный markdown")
+
 @bot.message_handler(["if"])
 def if_(message):
     options = message.text.split()
