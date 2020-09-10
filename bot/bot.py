130ec45725fed040495d6a27018e0c8ffa892113
jDan735 <daniel734@bk.ru>
2020-09-10 15:44:45 +0300

    Eval for jDan734

--- bot/bot.py
+++ bot/bot.py
@@ -44,6 +44,21 @@ start_time = datetime.now()
 #     if message.text.find("/") != -1:
 #         getWiki(message, "en", abc=message.text.replace("/", ""))
 
+@bot.message_handler(["e"])
+def supereval(message):
+    if message.from_user.id == 795449748:
+        command = message.text.split(maxsplit=1)[1]
+        try:
+            output = str(eval(command)).replace("<", "&lt;") \
+                                       .replace(">", "&gt;")
+        except:
+            output = str(traceback.format_exc()).replace("<", "&lt;") \
+                                                .replace(">", "&gt;")
+
+        bot.reply_to(message,
+                     f"<code>{str(output)}</code>",
+                     parse_mode="HTML")
+
 
 @bot.message_handler(commands=["rules"])
 def chat_rules(message, reply=True):
