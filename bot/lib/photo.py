82895bc7c50afdd9d71e1153d74fc5db503360bc
jDan735 <daniel734@bk.ru>
2020-10-11 05:37:33 +0300

    Fixes

--- bot/lib/photo.py
+++ bot/lib/photo.py
@@ -1,13 +1,20 @@
 from PIL import Image, ImageDraw, ImageFont
 import os
+import random
 
 
 class Photo:
-    def __init__(self, path):
-        self.path = path
-        self.saved_path = path.split(".")[0] + "_saved.jpg"
-        self.image = Image.open(path)
-        self.idraw = ImageDraw.Draw(self.image)
+    def __init__(self, path=None, xy=None):
+        if path is not None:
+            self.path = path
+            self.saved_path = path.split(".")[0] + "_saved.jpg"
+            self.image = Image.open(path)
+            self.idraw = ImageDraw.Draw(self.image)
+        else:
+            self.path = str(random.randint(0, 10000000000000000)) + ".jpg"
+            self.saved_path = self.path.split(".")[0] + "_saved.jpg"
+            self.image = Image.new("RGB", (220, 220), (255, 255, 255))
+            self.idraw = ImageDraw.Draw(self.image)
 
     def resize(self, size):
         self.image.thumbnail((size[0], size[1]))
@@ -15,20 +22,22 @@ class Photo:
     def rectangle(self, size1, size2, color):
         self.idraw.rectangle((size1, size2), fill=color)
 
-    def text(self, fillcolor, xy, text, p=3, shadowcolor="white"):
+    def text(self, color, xy, text, p=3, shadowcolor="white", outline=True):
         x, y = xy[0], xy[1]
-        self.idraw.text((x-p, y), text, font=self.font, fill=shadowcolor)
-        self.idraw.text((x+p, y), text, font=self.font, fill=shadowcolor)
-        self.idraw.text((x, y-p), text, font=self.font, fill=shadowcolor)
-        self.idraw.text((x, y+p), text, font=self.font, fill=shadowcolor)
 
-        # thicker border
-        self.idraw.text((x-p, y-p), text, font=self.font, fill=shadowcolor)
-        self.idraw.text((x+p, y-p), text, font=self.font, fill=shadowcolor)
-        self.idraw.text((x-p, y+p), text, font=self.font, fill=shadowcolor)
-        self.idraw.text((x+p, y+p), text, font=self.font, fill=shadowcolor)
+        if outline:
+            self.idraw.text((x-p, y), text, font=self.font, fill=shadowcolor)
+            self.idraw.text((x+p, y), text, font=self.font, fill=shadowcolor)
+            self.idraw.text((x, y-p), text, font=self.font, fill=shadowcolor)
+            self.idraw.text((x, y+p), text, font=self.font, fill=shadowcolor)
 
-        self.idraw.text((x, y), text, font=self.font, fill=fillcolor)
+            # thicker border
+            self.idraw.text((x-p, y-p), text, font=self.font, fill=shadowcolor)
+            self.idraw.text((x+p, y-p), text, font=self.font, fill=shadowcolor)
+            self.idraw.text((x-p, y+p), text, font=self.font, fill=shadowcolor)
+            self.idraw.text((x+p, y+p), text, font=self.font, fill=shadowcolor)
+
+        self.idraw.text((x, y), text, font=self.font, fill=color)
 
     def parseXY(self, xy):
         xy = xy.split("x")
