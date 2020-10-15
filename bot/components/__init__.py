9a59e7988fb1fe3b6eef2596798b622e77e42f5f
jDan735 <daniel734@bk.ru>
2020-10-15 23:36:40 +0300

    Fix for heroku

--- bot/components/__init__.py
+++ bot/components/__init__.py
@@ -1,7 +1,9 @@
+from .token import heroku
 from os import listdir
 
+files = listdir("bot/components") if heroku else listdir("components")
 __all__ = []
 
-for file in listdir("components"):
+for file in files:
     if file.endswith(".py") and not file == "__init__.py":
         __all__.append(file[:-3])
