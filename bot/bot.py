bc1ce1e23511a783add6f32fbe3d56d4c045c648
jDan735 <daniel734@bk.ru>
2020-10-16 00:02:04 +0300

    Fix ban

--- bot/bot.py
+++ bot/bot.py
@@ -2,27 +2,6 @@
 
 import traceback
 
-# import components.stickers
-# import components.text
-# import components.art
-# import components.habr
-# import components.youtube
-# import components.menu
-# import components.status
-# import components.wiki
-# import components.photo
-# import components.random
-# import components.rules
-# import components.memes
-# import components.mrakopedia
-# import components.wget
-# import components.lurk
-# import components.inline
-# import components.test
-# import components.math
-# import components.dev
-# import components.crypto
-# import components.ban
 from components import *
 from components.token import bot
 
--- bot/components/__init__.py
+++ bot/components/__init__.py
@@ -5,5 +5,8 @@ files = listdir("bot/components") if heroku else listdir("components")
 __all__ = []
 
 for file in files:
-    if file.endswith(".py") and not file == "__init__.py":
+    if file.endswith(".py") \
+       and not file == "__init__.py" \
+       and not file == "ban.py":
         __all__.append(file[:-3])
+__all__.append("ban")
