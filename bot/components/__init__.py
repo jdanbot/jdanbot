b0d645c08ea44ca3c757bf7918d9385ca8362c4b
jDan735 <daniel734@bk.ru>
2020-10-15 23:44:13 +0300

    Init debug

--- bot/components/__init__.py
+++ bot/components/__init__.py
@@ -4,6 +4,9 @@ from os import listdir
 files = listdir("bot/components") if heroku else listdir("components")
 __all__ = []
 
+print(f"{files = }")
+
 for file in files:
     if file.endswith(".py") and not file == "__init__.py":
+        print(f"{file = }")
         __all__.append(file[:-3])
