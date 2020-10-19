340aba7c2f030fe284fe1c6bccb83d8d0d97bee6
jDan735 <daniel734@bk.ru>
2020-10-19 03:26:57 +0300

    Try fix for locale opener

--- bot/components/texts.py
+++ bot/components/texts.py
@@ -1,4 +1,8 @@
 import yaml
 
-with open("texts.yml", encoding="UTF-8") as file:
-    texts = yaml.safe_load(file.read())
+try:
+    with open("texts.yml", encoding="UTF-8") as file:
+        texts = yaml.safe_load(file.read())
+except FileNotFoundError:
+    with open("../texts.yml", encoding="UTF-8") as file:
+        texts = yaml.safe_load(file.read())
