71490786da28ae64d3a20b48befee749424cfd7f
jDan735 <daniel734@bk.ru>
2020-10-20 08:35:54 +0300

    Add random to bashorg

--- bot/components/bash.py
+++ bot/components/bash.py
@@ -8,19 +8,15 @@ import requests
 def bashorg(message):
     params = message.text.split(maxsplit=2)
     if len(params) == 1:
-        bot.reply_to(message, "Введи номер цитаты из башорга")
+        r = requests.get("https://bash.im/random")
+    else:
+        try:
+            num = int(params[1])
+        except ValueError:
+            bot.reply_to(message, "Введи число")
+            return
 
-    try:
-        num = int(params[1])
-    except ValueError:
-        bot.reply_to(message, "Введи число")
-        return
-
-    try:
         r = requests.get(f"https://bash.im/quote/{num}")
-    except:
-        bot.reply_to(message, "Не удалось загрузить цитату")
-        return
 
     soup = BeautifulSoup(r.text.replace("<br>", "\n"), 'lxml')
     soup2 = soup.find("div", class_="quote__body")
