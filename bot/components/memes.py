ce1d4a9f61d62848ed253be94abd2df0391ef0c3
jDan735 <daniel734@bk.ru>
2020-10-20 07:25:49 +0300

    Optimizations in memes.py

--- bot/components/memes.py
+++ bot/components/memes.py
@@ -2,91 +2,60 @@ from .token import bot, heroku
 from .texts import texts
 
 
-@bot.message_handler(commands=["ban"])
-def ban(message):
+def send_meme(message, text):
     try:
         bot.delete_message(message.chat.id, message.message_id)
     except:
         pass
-    msg = message.text.replace("/ban@jDan734_bot", "").replace("/ban", "")
+
     try:
-        bot.send_message(message.chat.id, "Бан" + msg, reply_to_message_id=message.reply_to_message.message_id)
+        bot.send_message(message.chat.id, text,
+                         reply_to_message_id=message.reply_to_message.message_id)
     except AttributeError:
-        bot.send_message(message.chat.id, "Бан" + msg)
+        bot.send_message(message.chat.id, text)
+
+
+memes = {
+    "bylo": "Было",
+    "ne_bylo": "Не было",
+    "rzaka": texts["rzaka"],
+    "rzaka_full": texts["rzaka_full"],
+    "rzaka_time": f"{texts['rzaka_time']} часов"
+}
 
 
 @bot.message_handler(commands=["bylo"])
 def bylo(message):
-    try:
-        bot.delete_message(message.chat.id, message.message_id)
-    except:
-        pass
-
-    try:
-        bot.send_message(message.chat.id, "Было", reply_to_message_id=message.reply_to_message.message_id)
-    except AttributeError:
-        bot.send_message(message.chat.id, "Было")
+    send_meme(message, memes["bylo"])
 
 
 @bot.message_handler(commands=["ne_bylo"])
 def ne_bylo(message):
-    try:
-        bot.delete_message(message.chat.id, message.message_id)
-    except:
-        pass
-
-    try:
-        bot.send_message(message.chat.id, "Не было", reply_to_message_id=message.reply_to_message.message_id)
-    except AttributeError:
-        bot.send_message(message.chat.id, "Не было")
-
-
-@bot.message_handler(commands=["fake"])
-def polak(message):
-    try:
-        bot.delete_message(message.chat.id, message.message_id)
-    except:
-        pass
-
-    path = "bot/images/polak.jpg" if heroku else "../images/polak.jpg"
-
-    try:
-        bot.send_photo(message.chat.id, open(path, "rb"),
-                       reply_to_message_id=message.reply_to_message.message_id)
-    except AttributeError:
-        bot.send_photo(message.chat.id, open(path, "rb").read())
+    send_meme(message, memes["ne_bylo"])
 
 
 @bot.message_handler(commands=["rzaka"])
 def rzaka(message):
-    try:
-        bot.delete_message(message.chat.id, message.message_id)
-    except:
-        pass
-
-    try:
-        bot.send_message(message.chat.id,
-                         texts["rzaka"],
-                         reply_to_message_id=message.reply_to_message.message_id)
-    except AttributeError:
-        bot.send_message(message.chat.id, texts["rzaka"])
-
-
-@bot.message_handler(commands=["rzaka_time"])
-def rzaka(message):
-    bot.reply_to(message, str(texts["rzaka_time"]) + " часов")
+    send_meme(message, memes["rzaka"])
 
 
 @bot.message_handler(commands=["rzaka_full"])
 def rzaka_full(message):
-    try:
-        bot.delete_message(message.chat.id, message.message_id)
-    except:
-        pass
+    send_meme(message, memes["rzaka_full"])
 
-    try:
-        bot.send_message(message.chat.id,
-                         texts["rzaka_full"],
-                         reply_to_message_id=message.reply_to_message.message_id)
-    except AttributeError:
-        bot.send_message(message.chat.id, texts["rzaka_full"])
+
+@bot.message_handler(commands=["rzaka_time"])
+def rzaka_time(message):
+    send_meme(message, memes["rzaka_time"])
+
+
+@bot.message_handler(commands=["ban"])
+def ban(message):
+    msg = message.text.replace("/ban@jDan734_bot", "").replace("/ban", "")
+    send_meme(message, "Бан" + msg)
+
+
+@bot.message_handler(commands=["fake"])
+def fake(message):
+    path = "bot/images/polak.jpg" if heroku else "../images/polak.jpg"
+    send_meme(message, open(path, "rb").read())
