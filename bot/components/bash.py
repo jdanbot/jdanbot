19d28ed23a972ffb74262ab8ee1d3de2d3f735d2
jDan735 <daniel734@bk.ru>
2020-10-20 08:27:26 +0300

    Update bash.py, and uncomment

--- bot/components/bash.py
+++ bot/components/bash.py
@@ -1,12 +1,35 @@
-"""
-@bot.message_handler(commands=["bashorg"])
+from .token import bot
+from bs4 import BeautifulSoup
+
+import requests
+
+
+@bot.message_handler(commands=["bashorg", "bash", "b"])
 def bashorg(message):
-    num = int(message.text.replace("/bashorg@jDan734_bot ", "").replace("/bashorg ", ""))
-    r = requests.get(f"https://bash.im/quote/{num}")
-    soup = BeautifulSoup(r.text.replace("<br>", "БАН").replace("<br\\>", "БАН"), 'html.parser')
+    params = message.text.split(maxsplit=2)
+    if len(params) == 1:
+        bot.reply_to(message, "Введи номер цитаты из башорга")
 
-    print(soup.find("div", class_="quote__body").text.replace('<div class="quote__body">', "").replace("</div>", "").replace("<br\\>", "\n"))
+    try:
+        num = int(params[1])
+    except ValueError:
+        bot.reply_to(message, "Введи число")
+        return
 
-    soup2 = BeautifulSoup(soup.find("div", class_="quote__body"), "lxml")
+    try:
+        r = requests.get(f"https://bash.im/quote/{num}")
+    except:
+        bot.reply_to(message, "Не удалось загрузить цитату")
+        return
+
+    soup = BeautifulSoup(r.text.replace("<br>", "\n"), 'lxml')
+    soup2 = soup.find("div", class_="quote__body")
+
+    for tag in soup2.find_all("div", {"class": "quote__strips"}):
+        tag.replace_with("")
+
+    soup2 = soup2 .text \
+                  .replace('<div class="quote__body">', "") \
+                  .replace("</div>", "") \
+                  .replace("<br\\>", "\n")
     bot.reply_to(message, soup2)
-"""
