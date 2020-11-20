5c6099c52f24f1ed6f33ec799a95eb766dec8d6e
jDan735 <daniel734@bk.ru>
2020-11-20 19:40:32 +0300

    Add absurdowiki

---
+++ bot/components/absurd.py
@@ -0,0 +1,47 @@
+from .token import bot
+from .lib.lurkmore import Lurkmore
+
+a = Lurkmore()
+a.url = "https://absurdopedia.net/w/api.php"
+a.url2 = "https://absurdopedia.net/"
+
+
+@bot.message_handler(commands=["absurd"])
+def getAbsurd(message, logs=False):
+    options = message.text.split(maxsplit=1)
+    if len(options) == 1:
+        bot.reply_to(message, "Напишите название статьи")
+        return
+
+    name = options[1]
+    print(f"[Absurdowiki] {name}")
+
+    s = a.opensearch(name)
+
+    if len(s) == 0:
+        bot.reply_to(message, "Не найдено")
+        return
+
+    p = a.getPage(s)
+    i = a.getImagesList(s)
+
+    # for item in i:
+    #     bot.send_photo(message.chat.id, lurk.getImage(item), caption=item)
+
+    if len(i) != 0:
+        try:
+            image = a.getImage(i[0])
+            bot.send_chat_action(message.chat.id, "upload_photo")
+            bot.send_photo(message.chat.id,
+                           image,
+                           caption=a.parse(p)[:1000],
+                           parse_mode="HTML")
+        except Exception as e:
+            bot.send_chat_action(message.chat.id, "typing")
+            bot.reply_to(message, e)
+            try:
+                bot.reply_to(message, a.parse(p)[:4096], parse_mode="HTML")
+            except:
+                bot.reply_to(message, a.parse(p)[:4096])
+    else:
+        bot.reply_to(message, a.parse(p)[:4096], parse_mode="HTML")
