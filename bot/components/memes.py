82be4c082411f3e5541530dd8859b3d1b952922a
jDan735 <daniel734@bk.ru>
2020-12-06 15:54:06 +0300

    Add egg

--- bot/components/memes.py
+++ bot/components/memes.py
@@ -51,8 +51,40 @@ def rzaka_time(message):
 
 @bot.message_handler(commands=["java1"])
 def java_secret(message):
-    file = "bot/java.ogg" if heroku else "java.ogg" 
+    file = "bot/java.ogg" if heroku else "java.ogg"
     bot.send_voice(message.chat.id, open(file, "rb+"))
+
+
+@bot.message_handler(commands=["0x00001488"])
+def nazi_error(message):
+    bot.reply_to(message, """A problem has been detected and KanobuOS has been shut down to prevent damage
+to your computer.
+
+The problem seems to be caused by the following file: canobu.exe
+
+SYSTEM_BAN_EXCEPTION_NOT_HANDLED
+
+If this is the first time you've seen this stop error screen,
+restart your bot. If this screen appears again, follow
+these steps:
+
+Check to make sure any new hardware or software is properly installed.
+If this is a new installation, ask your hardware or software manufacturer
+for any KanobuOS updates you might need.
+
+If problems continue, disable or remove any newly installed hardware
+or software. Disable BAN memory options such as caching or shadowing.
+If you need to use safe mode to remove or disable components, restart
+your computer, press F to select Advanced Startup Options, and then
+select Safe Ban.
+
+Technical Information:
+
+*** STOP: 0x1000007e (0xffffffffc0000005, 0xfffff80002e55151, 0xfffff880009a99d8,
+0xfffff880009a9230)
+
+*** canobu.exe - Address 0x00001488 base at 0xfffff80002e0d000 DateStamp
+0x4ce7951a""")
 
 
 @bot.message_handler(commands=["ban"])
