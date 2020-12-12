b789d64c37d3b4cbba7c27474385c6cb29507827
jDan735 <daniel734@bk.ru>
2020-12-12 18:11:33 +0300

    Disable async for get_video_id

--- bot/youtube.py
+++ bot/youtube.py
@@ -2,7 +2,7 @@ from .bot import dp, bot
 import urllib
 
 
-async def get_video_id(url):
+def get_video_id(url):
     try:
         return urllib.parse.parse_qs(urllib.parse.urlparse(url).query)["v"][0]
     except Exception:
@@ -17,7 +17,7 @@ async def preview(message):
         url = message.text.split(maxsplit=1)[1]
 
     download_url = "https://img.youtube.com/vi/{id}/maxresdefault.jpg"
-    video_id = await get_video_id(url)
+    video_id = get_video_id(url)
 
     await bot.send_chat_action(message.chat.id, "upload_photo")
     await message.reply_photo(download_url.format(id=video_id))
