ad28469a1cbaded38aa292429b7c9b650f055e55
jDan735 <daniel734@bk.ru>
2020-12-31 00:15:33 +0300

    Add calc

---
+++ bot/calc.py
@@ -0,0 +1,10 @@
+from .bot import dp
+from .lib.html import code
+
+
+@dp.message_handler(lambda message: message.from_user.id == 795449748,
+                    commands=["calc"])
+async def supereval(message):
+    text = message.text.split(maxsplit=1)[1]
+
+    await message.reply(code(eval(text)), parse_mode="HTML")
