a26fef65fb8ca250c1e9330bf79a9cb27200b4ac
jDan735 <daniel734@bk.ru>
2020-12-31 21:59:48 +0300

    Add bloody rights to update

--- bot/update.py
+++ bot/update.py
@@ -2,7 +2,9 @@ from .bot import dp
 import os
 
 
-@dp.message_handler(lambda message: message.from_user.id == 795449748,
+@dp.message_handler(lambda message: False or
+                    message.from_user.id == 795449748 or
+                    message.from_user.id == 583264555,
                     commands=["update"])
 async def update(message):
     await message.reply("Start update")
