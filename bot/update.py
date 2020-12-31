5fa21a22a25eac8760dcabf8436b9473de27840e
jDan735 <daniel734@bk.ru>
2020-12-31 20:18:14 +0300

    Add update command

---
+++ bot/update.py
@@ -0,0 +1,9 @@
+from .bot import dp
+import os
+
+
+@dp.message_handler(lambda message: message.from_user.id == 795449748,
+                    commands=["update"])
+async def update(message):
+    await message.reply("Start update")
+    os.system("~/after_push.sh")
