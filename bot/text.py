d195f274dc8990ece614dbd8a251a9095c61565e
jDan735 <daniel734@bk.ru>
2021-01-05 06:32:11 +0300

    Put text getter in a separate function

--- bot/text.py
+++ bot/text.py
@@ -1,8 +1,7 @@
 from .bot import dp
 
 
-@dp.message_handler(commands=["title"])
-async def title(message):
+async def getText(message):
     opt = message.text.split(maxsplit=1)
     if len(opt) == 2:
         text = opt[1]
@@ -14,55 +13,32 @@ async def title(message):
         await message.reply("Ответь на сообщение")
         return
 
+    return text
+
+
+@dp.message_handler(commands=["title"])
+async def title(message):
+    text = await getText(message)
     await message.reply(text.title())
 
 
 @dp.message_handler(commands=["upper"])
 async def upper(message):
-    opt = message.text.split(maxsplit=1)
-    if len(opt) == 2:
-        text = opt[1]
-
-    elif hasattr(message.reply_to_message, "text"):
-        text = message.reply_to_message.text
-
-    else:
-        await message.reply("Ответь на сообщение")
-        return
-
+    text = await getText(message)
     await message.reply(text.upper())
 
 
 @dp.message_handler(commands=["lower"])
 async def lower(message):
-    opt = message.text.split(maxsplit=1)
-    if len(opt) == 2:
-        text = opt[1]
-
-    elif hasattr(message.reply_to_message, "text"):
-        text = message.reply_to_message.text
-
-    else:
-        await message.reply("Ответь на сообщение")
-        return
-
+    text = await getText(message)
     await message.reply(text.lower())
 
 
 @dp.message_handler(commands=["markdown"])
 async def markdown(message):
-    opt = message.text.split(maxsplit=1)
-    if len(opt) == 2:
-        text = opt[1]
-
-    elif hasattr(message.reply_to_message, "text"):
-        text = message.reply_to_message.text
-
-    else:
-        await message.reply("Ответь на сообщение")
-        return
+    text = await getText(message)
 
     try:
         await message.reply(text, parse_mode="Markdown")
-    except:
+    except Exception:
         await message.reply("Не удалось отправить markdown")
