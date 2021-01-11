c91d5baf890062fc7cae9af3ec17dd3d883e0dab
jDan735 <daniel734@bk.ru>
2021-01-12 02:09:26 +0300

    Run inline code

--- bot/inline.py
+++ bot/inline.py
@@ -1,16 +1,17 @@
-from .token import bot
+from .bot import bot, dp
 
-from wikipya.core import Wikipya
+from wikipya.aiowiki import Wikipya
 
-import telebot
+import traceback
+import aiogram
 import re
 
 
 digits_pattern = re.compile(r'.{,}', re.MULTILINE)
 
 
-@bot.inline_handler(func=lambda query: len(query.query) > 0)
-def query_text(query):
+@dp.inline_handler(lambda query: len(query.query) > 0)
+async def query_text(query):
     try:
         matches = re.match(digits_pattern, query.query)
 
@@ -20,34 +21,33 @@ def query_text(query):
     try:
         wiki = Wikipya("ru")
         q = matches.group()
-        r = wiki.search(q, 5)
-        print(q)
+        r = await wiki.search(q, 3)
+        print(r)
         btns = []
         for page in r:
-            p = wiki.getPage(page[0])
-            img = wiki.getImageByPageName(page[0], 75)
-            full_img = wiki.getImageByPageName(page[0])
-            t1 = re.sub("<b>", f'<a href="{full_img}">', wiki.parsePage(p), 1)
-            t2 = re.sub("</b>", "</a>", t1, 1)
+            p = await wiki.getPage(page[0])
+            img = await wiki.getImageByPageName(page[0], 75)
+            full_img = await wiki.getImageByPageName(page[0])
             if img != -1 and full_img != -1:
-                btns.append(telebot.types.InlineQueryResultArticle(
+                t1 = re.sub("<b>", f'<a href="{full_img["source"]}">', wiki.parsePage(p), 1)
+                t2 = re.sub("</b>", "</a>", t1, 1)
+                btns.append(aiogram.types.InlineQueryResultArticle(
                         id=page[1], title=page[0],
                         description=p.text[:100],
-                        input_message_content=telebot.types.InputTextMessageContent(
+                        input_message_content=aiogram.types.InputTextMessageContent(
                         message_text=t2,
                         parse_mode="HTML"),
-                        thumb_url=img, thumb_width=48, thumb_height=48
+                        thumb_url=img["source"], thumb_width=48, thumb_height=48
                 ))
             else:
-                btns.append(telebot.types.InlineQueryResultArticle(
+                btns.append(aiogram.types.InlineQueryResultArticle(
                         id=page[1], title=page[0],
                         description=p.text[:100],
-                        input_message_content=telebot.types.InputTextMessageContent(
+                        input_message_content=aiogram.types.InputTextMessageContent(
                         message_text=wiki.parsePage(p),
                         parse_mode="HTML"),
                         thumb_url="https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Wikipedia-logo-v2.svg/75px-Wikipedia-logo-v2.svg.png", thumb_width=48, thumb_height=48
                 ))
-
-        bot.answer_inline_query(query.id, btns)
+        await bot.answer_inline_query(query.id, btns)
     except Exception as e:
-        print(f"{type(e)}\n{str(e)}")
+        print(traceback.format_exc())
