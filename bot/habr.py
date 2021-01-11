6848cdc8c7d8f0e4b898086761d679c45bcef43c
jDan735 <daniel734@bk.ru>
2021-01-11 12:22:12 +0300

    Adapt for aiogram

--- bot/habr.py
+++ bot/habr.py
@@ -1,28 +1,28 @@
-from .token import bot
+from .bot import dp
 from .lib.habr import Habr
 
 
-@bot.message_handler(["habr"])
-def habr(message):
+@dp.message_handler(commands=["habr"])
+async def habr(message):
     options = message.text.split(maxsplit=1)
 
     if len(options) == 1:
-        bot.reply_to(message, "Введи id поста из хабра")
+        await message.reply("Введи id поста из хабра")
         return
 
     try:
         id_ = int(options[-1])
 
-    except:
-        bot.reply_to(message, "Введи валидный id поста")
+    except ValueError:
+        await message.reply("Введи валидный id поста")
 
     h = Habr()
 
     try:
-        bot.reply_to(message,
-                     h.page(id_)[:4096],
-                     parse_mode="HTML",
-                     disable_web_page_preview=True)
+        habrPage = await h.page(id_)
+        await message.reply(habrPage[:4096],
+                            parse_mode="HTML",
+                            disable_web_page_preview=True)
 
     except Exception as e:
-        bot.reply_to(message, e)
+        await message.reply(e)
--- bot/lib/habr.py
+++ bot/lib/habr.py
@@ -1,31 +1,24 @@
-import requests
-# import re
 from bs4 import BeautifulSoup
+from .aioget import aioget
 
 
 class Habr:
     def __init__(self):
         self.url = "https://habr.com/ru/post/"
 
-    def page(self, id_):
-        r = requests.get(f"{self.url}{id_}/")
-        soup = BeautifulSoup(r.text, "lxml")
-        page = ""
+    async def page(self, id_):
+        r = await aioget(f"{self.url}{id_}/")
+        soup = BeautifulSoup(await r.text(), "lxml")
 
-        page += f'<b>{soup.find("h1").span.text.upper()}</b>\n\n'
+        page = f'<b>{soup.find("h1").span.text.upper()}</b>\n\n'
         page += soup.findAll("div", {"id": "post-content-body"})[0].text
 
-        for tag in soup.findAll("div", {"id": "post-content-body"})[0].findAll("h2"):
+        for tag in soup.findAll("div", {"id": "post-content-body"})[0] \
+                       .findAll("h2"):
             page = page.replace(tag.text, f"\n<b>{tag.text}</b>")
 
-        for tag in soup.findAll("div", {"id": "post-content-body"})[0].findAll("h3"):
+        for tag in soup.findAll("div", {"id": "post-content-body"})[0] \
+                       .findAll("h3"):
             page = page.replace(tag.text, f"\n<b>{tag.text}</b>")
-        # page += str(soup.findAll("div", {"id": "post-content-body"})[0]) \
-        #             .replace("</div>", "") \
-        #             .replace("<blockquote>", "") \
-        #             .replace("</blockquote>", "") \
-        #             .replace("<br/>", "\n")
-
-        # page = re.sub(r'(\<div(/?[^>]+)>)', '', page)
 
         return page
