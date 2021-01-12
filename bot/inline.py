95fd663a84ba314c6ba1c5dee8f83a39634699d4
jDan735 <daniel734@bk.ru>
2021-01-12 05:34:14 +0300

    Translate code to pep8

--- bot/inline.py
+++ bot/inline.py
@@ -1,10 +1,12 @@
 from .bot import bot, dp
+from .data import data
 
 from wikipya.aiowiki import Wikipya
 
 import traceback
-import aiogram
 import re
+
+from aiogram.types import InputTextMessageContent, InlineQueryResultArticle
 
 
 digits_pattern = re.compile(r'.{,}', re.MULTILINE)
@@ -14,40 +16,41 @@ digits_pattern = re.compile(r'.{,}', re.MULTILINE)
 async def query_text(query):
     try:
         matches = re.match(digits_pattern, query.query)
+        q = matches.group()
 
-    except AttributeError as ex:
+    except AttributeError:
         return
 
     try:
         wiki = Wikipya("ru")
-        q = matches.group()
-        r = await wiki.search(q, 3)
-        print(r)
+        r = await wiki.search(q, 7)
+
         btns = []
         for page in r:
             p = await wiki.getPage(page[0])
             img = await wiki.getImageByPageName(page[0], 75)
             full_img = await wiki.getImageByPageName(page[0])
+            t = wiki.parsePage(p)
+
+            btn_defaults = {"id": page[1], "title": page[0],
+                            "description": p.text[:100],
+                            "thumb_width": 48, "thumb_height": 48,
+                            "input_message_content": InputTextMessageContent(
+                                message_text=t,
+                                parse_mode="HTML"
+                            )}
+
             if img != -1 and full_img != -1:
-                t1 = re.sub("<b>", f'<a href="{full_img["source"]}">', wiki.parsePage(p), 1)
-                t2 = re.sub("</b>", "</a>", t1, 1)
-                btns.append(aiogram.types.InlineQueryResultArticle(
-                        id=page[1], title=page[0],
-                        description=p.text[:100],
-                        input_message_content=aiogram.types.InputTextMessageContent(
-                        message_text=t2,
-                        parse_mode="HTML"),
-                        thumb_url=img["source"], thumb_width=48, thumb_height=48
-                ))
+                btns.append(InlineQueryResultArticle(
+                                **btn_defaults,
+                                thumb_url=img["source"]
+                            ))
             else:
-                btns.append(aiogram.types.InlineQueryResultArticle(
-                        id=page[1], title=page[0],
-                        description=p.text[:100],
-                        input_message_content=aiogram.types.InputTextMessageContent(
-                        message_text=wiki.parsePage(p),
-                        parse_mode="HTML"),
-                        thumb_url="https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Wikipedia-logo-v2.svg/75px-Wikipedia-logo-v2.svg.png", thumb_width=48, thumb_height=48
-                ))
+                btns.append(InlineQueryResultArticle(
+                                **btn_defaults,
+                                thumb_url=data["default_wiki_image"]
+                            ))
+
         await bot.answer_inline_query(query.id, btns)
-    except Exception as e:
+    except Exception:
         print(traceback.format_exc())
