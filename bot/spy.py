e64e51927e90a423b406f89078b76b9ebec44ca8
jDan735 <daniel734@bk.ru>
2021-01-19 17:17:35 +0300

    Add spy

--- bot/spy.py
+++ bot/spy.py
@@ -1,5 +1,7 @@
+from .lib.prettyword import prettyword
 from .bot import dp, conn
 from .lib.html import code
+from .data import data
 
 
 @dp.message_handler(commands=["leave_from_every"])
@@ -24,13 +26,58 @@ async def leaveFromAll(message):
             await message.reply("Вы не регистрировались")
         else:
             cur.execute(f"DELETE from c{chatid} where id={user['id']}")
-            await message.reply("Вас больше нет в бд)))")
+            conn.commit()
+            await message.reply("Вас больше нет в бд")
     except Exception as err:
         await message.reply(err)
 
 
-@dp.message_handler(commands=["every"])
-async def testAll(message):
+def getUniqueUsers(cur, users, chatid):
+    try:
+        e = cur.execute("SELECT * FROM c" + chatid).fetchall()
+
+        for user in e:
+            if user[0] in users:
+                pass
+            else:
+                users.append(user[0])
+
+        currentUsers = e
+    except Exception:
+        currentUsers = None
+
+    return [users, currentUsers]
+
+
+def getPrettyUsersName(num):
+    return prettyword(num, data["users"])
+
+
+@dp.message_handler(lambda message: message.from_user.id == 795449748,
+                    commands=["calc_all_bot_users"])
+async def calc_all_bot_users(message):
+    chatid = str(message.chat.id).replace("-", "_")
+    users = []
+    cur = conn.cursor()
+
+    chatUsers = getUniqueUsers(cur, [], chatid)[1]
+
+    e = cur.execute("SELECT * FROM sqlite_master;").fetchall()
+    for table in e:
+        users = getUniqueUsers(cur, users, table[1][1:])[0]
+
+    # В этом чяте <num> пользователей
+    # Всего <num> пользователей
+
+    text = "В этом чяте {} {}\nВсего {} {}"
+
+    await message.reply(text.format(len(chatUsers),
+                                    getPrettyUsersName(len(chatUsers)),
+                                    len(users),
+                                    getPrettyUsersName(len(users))))
+
+
+async def activateSpy(message):
     try:
         name = " ".join([message.from_user.first_name,
                          message.from_user.last_name])
@@ -55,17 +102,9 @@ async def testAll(message):
     e = cur.execute(f"{select} c{chatid} where id={user['id']}")
 
     if e.fetchone() is None:
-        await message.reply("*звуки включения зондов*")
         cur.execute('INSERT INTO c{chatid} VALUES ({id}, "{name}")'
                     .format(chatid=chatid, **user))
         conn.commit()
+        await message.reply("Чел, ты в списке бана")
     else:
         pass
-
-    e = cur.execute("SELECT * FROM c" + chatid)
-    users = []
-    for user in e:
-        users.append(f"[{user[1]}](tg://user?id={user[0]})")
-
-    await message.reply(", ".join(users),
-                        parse_mode="MarkdownV2")
