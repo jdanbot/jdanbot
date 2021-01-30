0a2073ef22475ef0bfd638403b83eb43cfca49c6
jDan735 <daniel734@bk.ru>
2021-01-31 00:26:22 +0300

    Add /cum egg

--- bot/eggs.py
+++ bot/eggs.py
@@ -17,6 +17,13 @@ async def cool_secret(message):
                          reply_to_message_id=message.message_id)
 
 
+@dp.message_handler(commands=["cum"])
+async def cool_secret(message):
+    await bot.send_voice(message.chat.id,
+                         open("music/cum.ogg", "rb+"),
+                         reply_to_message_id=message.message_id)
+
+
 @dp.message_handler(commands=["0x00001488"])
 async def secret_error(message):
     await message.reply(code(data["errors"]["egg_error"]),
