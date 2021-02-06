6c8a631cfe17e0ab27f92eca61a3578a1d422641
jDan735 <daniel734@bk.ru>
2021-02-06 10:14:02 +0300

    Fix wikipedia inline

--- bot/inline.py
+++ bot/inline.py
@@ -1,78 +1,63 @@
 from .bot import bot, dp
 from .data import data
 
-from wikipya.aiowiki import Wikipya
-
+from aiogram.types import InputTextMessageContent, \
+                          InlineQueryResultAudio, InlineQueryResultArticle
+from wikipya.aiowiki import Wikipya, NotFound
+from bs4 import BeautifulSoup
 
 from .lib import chez
+
 import traceback
 import re
 
-from aiogram.types import InlineQueryResultDocument, InputTextMessageContent, \
-                          InlineQueryResultAudio, InlineQueryResultArticle
 
-
-digits_pattern = re.compile(r'.{,}', re.MULTILINE)
-
-
-@dp.inline_handler(lambda query: query.query.startswith("wiki ") or
-                                 query.query.startswith("вики "))  # noqa: E127
+@dp.inline_handler(lambda query: False or
+                   query.query.startswith("wiki ") or
+                   query.query.startswith("вики "))
 async def query_text(query):
-    print(query.query)
-    try:
-        matches = re.match(digits_pattern, query.query)
-        q = matches.group()
+    q = query.query.split(maxsplit=1)[1]
 
-    except AttributeError:
-        return
+    wiki = Wikipya("ru")
+    search = await wiki.search(q, 3)
 
-    try:
-        wiki = Wikipya("ru")
-        search = await wiki.search(q, 15)
+    buttons = []
+    for item in search:
+        page = await wiki.page(item)
+        soup = BeautifulSoup(page.text, "lxml")
+        text = page.fixed
 
-        buttons = []
-        for page in search:
-            html = await wiki.getPage(page[0])
-            img = await wiki.getImageByPageName(page[0], 75)
-            full_img = await wiki.getImageByPageName(page[0])
-            text = wiki.parsePage(html)
+        btn_defaults = {"id": page.pageid, "title": page.title,
+                        "description": soup.text[:100],
+                        "thumb_width": 48, "thumb_height": 48,
+                        "input_message_content": InputTextMessageContent(
+                            message_text=text,
+                            parse_mode="HTML"
+                        )}
 
-            btn_defaults = {"id": page[1], "title": page[0],
-                            "description": html.text[:100],
-                            "thumb_width": 48, "thumb_height": 48,
-                            "input_message_content": InputTextMessageContent(
-                                message_text=text,
-                                parse_mode="HTML"
-                            )}
+        try:
+            img = await page.image(75)
+            buttons.append(InlineQueryResultArticle(**btn_defaults,
+                                                    thumb_url=img.source))
 
-            if img != -1 and full_img != -1:
-                buttons.append(InlineQueryResultArticle(
-                                   **btn_defaults,
-                                   thumb_url=img["source"]
-                               ))
-            else:
-                buttons.append(InlineQueryResultArticle(
-                                   **btn_defaults,
-                                   thumb_url=data["default_wiki_image"]
-                               ))
+        except NotFound:
+            default_image = data["default_wiki_image"]
+            buttons.append(InlineQueryResultArticle(**btn_defaults,
+                                                    thumb_url=default_image))
 
-        await bot.answer_inline_query(query.id, buttons)
-    except Exception:
-        print(traceback.format_exc())
+    await bot.answer_inline_query(query.id, buttons)
 
 
 @dp.inline_handler(lambda query: len(query.query) > 0)
 async def query_say(query):
-    print(f"{query.query = }")
     query.query = query.query.strip()
 
     if query.query.endswith(".") or \
        query.query.endswith("?") or \
        query.query.endswith("!"):
         btns = [
-            InlineQueryResultAudio(
-                id=1, title="test", audio_url=chez.say(query.query)
-            )
+            InlineQueryResultAudio(id=1, title="test",
+                                   audio_url=chez.say(query.query))
         ]
 
         await bot.answer_inline_query(query.id, btns)
