7eb4ee74f5e52ad9d0eb32aa6bd1038bb3190050
jDan735 <daniel734@bk.ru>
2021-02-07 05:54:55 +0300

    Fix paddings

--- bot/lib/cutecrop.py
+++ bot/lib/cutecrop.py
@@ -2,5 +2,14 @@ def cuteCrop(page, limit=1, text=""):
     for p in page.split("\n"):
         if len(text) + len(p) + 2 <= limit:
             text += p + "\n"
-    
-    return text
+
+    clean_text = []
+    text_splited = text.split("\n")
+
+    for p in text_splited:
+        if p == "":
+            pass
+        else:
+            clean_text.append(p)
+
+    return '\n\n'.join(clean_text)
