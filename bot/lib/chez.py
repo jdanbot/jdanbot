f3f15e95f4a9e05f8766dedd79c7500f7a3dd60e
Evgeny ChezRD Rumiantsev <chezrd@gmail.com>
2021-02-13 15:08:48 +0300

    updated tts settings

--- bot/lib/chez.py
+++ bot/lib/chez.py
@@ -3,10 +3,10 @@ from requests import Request
 def say(text):
     host = "https://tts.chez.work/say"
     params = {"text": "{text}".format(text=text),
-              "voice": "irina",
+              "voice": "aleksandr",
               "format": "opus",
-              "rate": "80",
-              "pitch": "80",
-              "volume": "50"}
+              "rate": "55",
+              "pitch": "10",
+              "volume": "70"}
 
     return Request("GET", host, params=params).prepare().url
