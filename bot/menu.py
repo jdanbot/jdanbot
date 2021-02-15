e7280a31f46bfd6f948ee4dc46540fac285f8d30
jDan735 <daniel734@bk.ru>
2021-02-16 01:28:53 +0300

    Small refactoring in menu.py

--- bot/menu.py
+++ bot/menu.py
@@ -1,8 +1,7 @@
-from .config import dp, bot
-from .data import data
-
 import aiogram
 
+from .config import bot, dp
+from .data import data
 
 keyboard = aiogram.types.InlineKeyboardMarkup()
 
@@ -21,7 +20,7 @@ for ind, button in enumerate(buttons):
     buttons[ind] = aiogram.types.InlineKeyboardButton(text=button[0],
                                                       callback_data=button[1])
 
-for ind, button in enumerate(buttons):
+for ind, _button in enumerate(buttons):
     a = buttons[ind:ind + 2]
     buttons.remove(a[1])
     keyboard.add(*a)
@@ -35,39 +34,12 @@ async def menu(message):
 
 @dp.callback_query_handler(lambda call: True)
 async def callback_worker(call):
-    if call.data == "main":
-        await edit(call, data.menu.main)
-
-    if call.data == "math":
-        await edit(call, data.menu.math)
-
-    if call.data == "text":
-        await edit(call, data.menu.text)
-
-    if call.data == "network":
-        await edit(call, data.menu.network)
-
-    if call.data == "math":
-        await edit(call, data.menu.math)
-
-    if call.data == "images":
-        await edit(call, data.menu.images)
-
-    if call.data == "commands":
-        await edit(call, data.menu.commands)
-
-    if call.data == "crypt":
-        await edit(call, data.menu.crypt)
-
-    if call.data == "memes":
-        await edit(call, data.menu.memes)
+    if call.data in list(data.menu._dict.keys()):
+        await edit(call, data.menu._dict[call.data])
 
 
 async def edit(call, text):
-    try:
-        await bot.edit_message_text(chat_id=call.message.chat.id,
-                                    message_id=call.message.message_id,
-                                    text=text, parse_mode="HTML",
-                                    reply_markup=keyboard)
-    except Exception:
-        pass
+    await bot.edit_message_text(chat_id=call.message.chat.id,
+                                message_id=call.message.message_id,
+                                text=text, parse_mode="HTML",
+                                reply_markup=keyboard)
