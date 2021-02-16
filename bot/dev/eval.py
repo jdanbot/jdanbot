258f6707c6ccc11a7ab383910029105af6f61ac5
jDan735 <daniel734@bk.ru>
2021-02-16 22:52:13 +0300

    Refactiring of /e

--- bot/dev/eval.py
+++ bot/dev/eval.py
@@ -1,43 +1,25 @@
-from ..config import bot, dp
-from ..lib.html import code
-
 import traceback
 import subprocess
 
-
-async def print_(message, text):
-    await message.reply(code("[print] " + str(text)), parse_mode="HTML")
+from ..config import bot, dp
+from ..lib.html import code
 
 
 @dp.message_handler(lambda message: message.from_user.id == 795449748,
                     commands=["e"])
 async def supereval(message):
-    text = message.text.split(maxsplit=1)[1]
+    code = message.text.split(maxsplit=1)[1]
 
-    async_ = "await" in text
+    exec(
+        "async def __ex(message, bot): " +
+        "".join(f"\n {L}" for L in code.split("\n"))
+    )
 
-    if async_:
-        try:
-            exec(
-                "async def __ex(message, bot, print): " +
-                "".join(f"\n {L}" for L in text.split("\n"))
-            )
-            await locals()["__ex"](message, bot, print_)
-        except Exception:
-            await message.reply(code(traceback.format_exc()),
-                                parse_mode="HTML")
-    else:
-        try:
-
-            exec(
-                "def __ex(message, bot): " +
-                "".join(f"\n {L}" for L in text.split("\n"))
-            )
-            locals()["__ex"](message, bot)
-
-        except Exception:
-            await message.reply(code(traceback.format_exc()),
-                                parse_mode="HTML")
+    try:
+        await locals()["__ex"](message, bot)
+    except Exception:
+        await message.reply(code(traceback.format_exc()),
+                            parse_mode="HTML")
 
 
 @dp.message_handler(lambda message: message.from_user.id == 795449748,
