8e842c1bf379bb9c0338d2eb9f55d32c9e69b312
jDan735 <daniel734@bk.ru>
2021-02-22 15:19:19 +0300

    Rewrite config.py

--- bot/config.py
+++ bot/config.py
@@ -1,46 +1,32 @@
-import coloredlogs
+import json
 import logging
 import sqlite3
-import json
-
-from os import environ
 from datetime import datetime
+from os import environ
+
+import coloredlogs
 from aiogram import Bot, Dispatcher
 
+try:
+    with open("config.json") as file:
+        config = json.loads(file)
+except FileNotFoundError:
+    config = {}
 
-if "TOKEN" in environ:
-    TOKEN = environ["TOKEN"]
+DATABASE_PATH = "jdandb.db"
+IMAGE_PATH = "bot/cache/{image}.jpg"
+START_TIME = datetime.now()
 
-    try:
-        bot_status = environ["STATUS"]
-    except:
-        print("Enter status of bot in token.json")
-else:
-    try:
-        with open("config.json") as file:
-            config = json.loads(file.read())
-            TOKEN = config["token"]
+TOKEN = environ.get("TOKEN") or config.get("token")
+STATUS = environ.get("STATUS") or config.get("status") or "unknown"
 
-            try:
-                bot_status = config["status"]
-            except KeyError:
-                print("Enter status of bot in token.json")
-                bot_status = "unknown"
-
-    except:
-        print("Create config.json with bot token")
-        exit()
-
-start_time = datetime.now()
 logging.basicConfig(level=logging.INFO)
-
 logger = logging.getLogger(__name__)
 coloredlogs.install(fmt="%(asctime)s %(levelname)s %(message)s",
                     level="INFO",
                     logger=logger)
-log = logger.info
 
-conn = sqlite3.connect("jdandb.db")
+conn = sqlite3.connect(DATABASE_PATH)
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
