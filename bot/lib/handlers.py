a1c94153b40db7c0776fe8fd951245939b49db20
jDan735 <daniel734@bk.ru>
2021-02-22 19:05:21 +0300

    Add handlers, rewrite photo.py

---
+++ bot/lib/handlers.py
@@ -0,0 +1,55 @@
+from ..config import bot, IMAGE_PATH
+from ..data import data
+from .photo import Photo
+
+
+def parse_arguments(limit, without_params=False):
+    def argument_wrapper(func):
+        async def wrapper(message):
+            params = message.text.split(maxsplit=limit)
+
+            if len(params) < limit and not without_params:
+                await message.reply(data.errors.few_args.format(num=limit),
+                                    parse_mode="Markdown")
+            else:
+                await func(message, params)
+
+        return wrapper
+
+    return argument_wrapper
+
+
+def get_reply_photo(func):
+    async def wrapper(message, params):
+        reply = message.reply_to_message
+        file_id = ""
+
+        if reply:
+            if reply.photo:
+                file_id = reply.photo[-1].file_id
+            elif reply.document:
+                file_id = reply.document.thumb.file_id
+        else:
+            await message.reply("Ответьте на фото командой")
+            return
+
+        photo = await bot.get_file(file_id)
+        file = await bot.download_file(photo.file_path)
+
+        await func(message, params, [file_id, file])
+
+    return wrapper
+
+
+def init_photo_file(func):
+    @get_reply_photo
+    async def wrapper(message, params, photo):
+        path = IMAGE_PATH.format(image=photo[0])
+
+        with open(path, "wb") as new_file:
+            new_file.write(photo[1].read())
+
+        img = Photo(path)
+        await func(message, params, img)
+
+    return wrapper
--- bot/photo.py
+++ bot/photo.py
@@ -1,37 +1,16 @@
 from random import choice
 
 from .config import bot, dp
-from .data import data
-from .lib.telegram import Telegram
+from .lib import handlers
 from .lib.html import bold, code
 from .lib.photo import Photo
 
 
 @dp.message_handler(commands=["resize"])
-async def resize(message):
-    tg = Telegram(bot)
-
-    params = tg.parse(message, 3)
-
-    if params == 404:
-        await message.reply(data.errors.few_args.format(num=3),
-                            parse_mode="Markdown")
-        return
-
+@handlers.parse_arguments(3)
+@handlers.init_photo_file
+async def resize(message, params, img):
     print(params)
-
-    try:
-        photo = await tg.photo(message)
-    except AttributeError:
-        await message.reply("Ответьте на фото")
-        return
-
-    file = await bot.download_file(photo[1].file_path)
-
-    with open(f"cache/{photo[0]}.jpg", "wb") as new_file:
-        new_file.write(file.read())
-
-    img = Photo(f"cache/{photo[0]}.jpg")
 
     try:
         img.resize([int(params[1]), int(params[2])])
@@ -45,30 +24,11 @@ async def resize(message):
 
 
 @dp.message_handler(commands=["text"])
-async def text(message):
-    tg = Telegram(bot)
-
-    params = message.text.split(" ", maxsplit=4)
-
-    if params == 404:
-        await message.reply(data.errors.few_args.format(num=4),
-                            parse_mode="Markdown")
-        return
-
+@handlers.parse_arguments(4)
+@handlers.init_photo_file
+async def text(message, params, img):
     print(params)
 
-    try:
-        photo = await tg.photo(message)
-    except AttributeError:
-        await message.reply("Ответьте на фото")
-        return
-
-    file = await bot.download_file(photo[1].file_path)
-
-    with open(f"cache/{photo[0]}.jpg", "wb") as new_file:
-        new_file.write(file.read())
-
-    img = Photo(f"cache/{photo[0]}.jpg")
     try:
         img.font("fonts/OpenSans-Bold.ttf", int(params[1]))
         img.text(params[2], img.parseXY(params[3]), params[4])
@@ -82,30 +42,11 @@ async def text(message):
 
 
 @dp.message_handler(commands=["t"])
-async def t(message):
-    tg = Telegram(bot)
-
-    params = message.text.split(" ", maxsplit=1)
-
-    if params == 404:
-        await message.reply(data.errors.few_args.format(num=2),
-                            parse_mode="Markdown")
-        return
-
+@handlers.parse_arguments(2)
+@handlers.init_photo_file
+async def t(message, params, img):
     print(params)
 
-    try:
-        photo = await tg.photo(message)
-    except AttributeError:
-        await message.reply("Ответьте на фото")
-        return
-
-    file = await bot.download_file(photo[1].file_path)
-
-    with open(f"cache/{photo[0]}.jpg", "wb") as new_file:
-        new_file.write(file.read())
-
-    img = Photo(f"cache/{photo[0]}.jpg")
     try:
         img.font("fonts/OpenSans-Bold.ttf", 75)
         img.text("black", (50, 50), params[1])
@@ -119,30 +60,10 @@ async def t(message):
 
 
 @dp.message_handler(commands=["rectangle"])
-async def rectangle(message):
-    tg = Telegram(bot)
-
-    params = tg.parse(message, 4)
-
-    if params == 404:
-        await message.reply(data.errors.few_args.format(num=4),
-                            parse_mode="Markdown")
-        return
-
+@handlers.parse_arguments(4)
+@handlers.init_photo_file
+async def rectangle(message, params, img):
     print(params)
-
-    try:
-        photo = await tg.photo(message)
-    except AttributeError:
-        await message.reply("Ответьте на фото")
-        return
-
-    file = await bot.download_file(photo[1].file_path)
-
-    with open(f"cache/{photo[0]}.jpg", "wb") as new_file:
-        new_file.write(file.read())
-
-    img = Photo(f"cache/{photo[0]}.jpg")
 
     try:
         img.rectangle(img.parseXY(params[2]),
@@ -158,21 +79,21 @@ async def rectangle(message):
 
 
 @dp.message_handler(commands=["random_color", "color"])
-async def random_color(message):
-    options = message.text.split(" ", maxsplit=1)
-    if len(options) == 1:
+@handlers.parse_arguments(2, without_params=True)
+async def random_color(message, params):
+    if len(params) == 1:
         randlist = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
                     "A", "B", "C", "D", "E", "F"]
 
         color = ""
-        for i in range(0, 6):
+        for _ in range(0, 6):
             color += str(choice(randlist))
 
         color = "#" + color
 
     else:
-        if options[1].startswith("#"):
-            color = options[1]
+        if params[1].startswith("#"):
+            color = params[1]
         else:
             await message.reply("Вводи цвет в формате hex")
             return
