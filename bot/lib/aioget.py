73091af7d9f85ca5cebb7bb97e978c3a6d4379a1
jDan735 <daniel734@bk.ru>
2021-03-02 05:12:57 +0300

    Add timeout to aioget

--- bot/lib/aioget.py
+++ bot/lib/aioget.py
@@ -1,11 +1,13 @@
 import aiohttp
 
 
-async def aioget(url, params={}):
+async def aioget(url, params={}, timeout=10):
     async with aiohttp.ClientSession() as session:
         try:
-            response = await session.get(url, params=params)
+            response = await session.get(url, params=params,
+                                         timeout=timeout)
         except aiohttp.client_exceptions.InvalidURL:
-            response = await session.get(f"https://{url}", params=params)
+            response = await session.get(f"https://{url}", params=params,
+                                         timeout=timeout)
 
         return response
