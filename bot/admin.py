4317e34569302c06be8917206a47891abcaef5c5
jDan735 <daniel734@bk.ru>
2021-03-09 22:38:39 +0300

    Add description and time to ban_template

--- bot/admin.py
+++ bot/admin.py
@@ -6,11 +6,19 @@ from .config import bot, dp
 
 @dp.message_handler(commands=["mute"])
 @handlers.only_admins
-@handlers.parse_arguments(2)
+@handlers.parse_arguments(3, True)
 async def admin_mut(message, params):
-    ban_time = time.time() + (int(params[1]) * 60)
+    ban_time = int(params[1]) * 60
     reply = message.reply_to_message
     await bot.restrict_chat_member(message.chat.id, reply.from_user.id,
-                                   until_date=ban_time)
+                                   until_date=time.time() + ban_time)
 
-    await message.reply("Светлая ему память)")
+    ban_template = "Выдан мут на имя <b>{name}</b>\n\n" + \
+                   "<b>Причина:</b> {why}\n" + \
+                   "<b>Срок:</b> <code>{time}</code> минут"
+
+    await message.reply(ban_template.format(
+        name=message.from_user.full_name,
+        why=params[-1] if len(params) == 3 else "не указана",
+        time=params[1]
+    ), parse_mode="HTML")
