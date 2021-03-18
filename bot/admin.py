3706001c8087483ae7363e64d04283090c81b339
Aphanasiy <me@aphanasiy.ru>
2021-03-18 21:58:13 +0300

    tiny bans refactor with datetime

--- bot/admin.py
+++ bot/admin.py
@@ -16,12 +16,8 @@ async def admin_mut(message, params):
     try:
         ban_time = float(params[1])
     except ValueError:
-        date = params[1].split(":")
-
-        hour = (int(date[0]) - datetime.now(TIMEZONE).hour) * 60
-        minute = int(date[1]) - datetime.now(TIMEZONE).minute
-
-        ban_time = hour + minute
+        bt = datetime.time.fromisoformat(params[1])
+        ban_time = bt.hour + bt.minute
 
     until_date = time.time() + ban_time * 60
     await bot.restrict_chat_member(message.chat.id, reply.from_user.id,
@@ -52,6 +48,9 @@ async def admin_mut(message, params):
                                parse_mode="HTML")
     except Exception:
         await message.reply(ban_log, parse_mode="HTML")
+
+
+
 
 
 def calc_ban_time(time):
