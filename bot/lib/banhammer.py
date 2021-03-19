4b38eadfdfc34c1bc7391e09ebc0087c1a106554
jDan735 <daniel734@bk.ru>
2021-03-19 23:04:02 +0300

    Tiny changes

--- bot/lib/banhammer.py
+++ bot/lib/banhammer.py
@@ -73,17 +73,6 @@ async def warn(
                                 period=datetime.timedelta(hours=23))
     wtbans += 1
 
-    if wtbans >= WARNS_TO_BAN:
-        await ban(blocker_message, blockable_message, "1440",
-                  f"Получено {wtbans}-е предупреждение")
-    else:
-        await mark_chat_member(blockable_message.from_user.id,
-                               blockable_message.chat.id,
-                               blocker_message.from_user.id,
-                               reason=reason)
-        await blocker_message.delete()
-
-
     warn_log = locale.warn_template.format(
         name=blockable_message.from_user.full_name,
         banchik=blocker_message.from_user.full_name,
@@ -92,6 +81,15 @@ async def warn(
         i=wtbans
     )
 
-    await bot.send_message(blockable_message.chat.id, warn_log,
-                           reply_to_message_id=blockable_message.message_id,
-                           parse_mode="HTML")
+    await blockable_message.reply(warn_log, parse_mode="HTML")
+
+    if wtbans >= WARNS_TO_BAN:
+        await ban(blocker_message, blockable_message, "1440",
+                  f"получено {wtbans}-е предупреждение")
+    else:
+        await mark_chat_member(blockable_message.from_user.id,
+                               blockable_message.chat.id,
+                               blocker_message.from_user.id,
+                               reason=reason)
+        await blocker_message.delete()
+
