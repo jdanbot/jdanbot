1ceb56cef22ccfd65a363e2dbc838a9ac2d4663a
jDan735 <daniel734@bk.ru>
2021-03-19 19:36:41 +0300

    Add sql schemas

---
+++ db.sql
@@ -0,0 +1,16 @@
+CREATE TABLE events (
+    chatid INTEGER,
+    id INTEGER,
+    name TEXT
+);
+
+CREATE TABLE notes (
+    chatid INTEGER,
+    name TEXT,
+    content TEXT
+);
+
+CREATE TABLE videos (
+    channelid TEXT,
+    links TEXT
+);
