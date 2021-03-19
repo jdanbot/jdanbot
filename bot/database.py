5a87f9187ac3d769e43d1d67a6623eec462de922
Daniel Zakharov <daniel734@bk.ru>
2021-03-19 20:50:03 +0300

    Fix: change arguments spaces

--- bot/database.py
+++ bot/database.py
@@ -13,11 +13,8 @@ conn = sqlite3.connect("jdanbot.db")
 warns = sqlfocus.SQLTable("warns", conn)
 
 
-async def count_wtbans(
-            user_id,
-            chat_id,
-            period=datatime.timedelta(hours=24)
-        ):
+async def count_wtbans(user_id, chat_id,
+                       period=datatime.timedelta(hours=24)):
     period_bound = int((datatime.datatime.now() - period).timestamp())
     w = warns.select(where=[f"timestamp >= {period_bound}", f"{user_id = }", f"{chat_id = }"])
     return len(w)
