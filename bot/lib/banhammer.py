706515cb42165f1bce52de57e69cfdf316b024c1
Daniel Zakharov <daniel734@bk.ru>
2021-03-19 20:54:42 +0300

    Fix: comma

--- bot/lib/banhammer.py
+++ bot/lib/banhammer.py
@@ -69,7 +69,7 @@ async def warn(
     wtbans = count_wtbans(blockable_message.from_user.id, 
                           blockable_message.chat.id, 
                           period=datatime.timedelta(hours=24))
-    mark_chat_member(blockable_message.from_user.id
+    mark_chat_member(blockable_message.from_user.id,
                      blockable_message.chat.id, 
                      blocker_message.from_user.id
                      reason=reason
