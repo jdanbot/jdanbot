7f65a4cbad6905cf791dd60e8bbe3c20695f42a9
Aphanasiy <me@aphanasiy.ru>
2021-03-19 20:41:36 +0300

    some other refactor

--- bot/database.py
+++ bot/database.py
@@ -19,7 +19,7 @@ async def count_wtbans(
             period=datatime.timedelta(hours=24)
         ):
     period_bound = int((datatime.datatime.now() - period).timestamp())
-    w = warns.select(where=[f"timestamp >= {period_bound}", f"user_id = {user_id}", f"chat_id = {chat_id}"])
+    w = warns.select(where=[f"timestamp >= {period_bound}", f"{user_id = }", f"{chat_id = }"])
     return len(w)
 
 async def mark_chat_member(user_id, chat_id, admin_id, reason):
