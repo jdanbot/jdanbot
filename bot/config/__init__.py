a318e222be9d3d1a356d30b87015b6c6ac7f3e45
jDan735 <daniel734@bk.ru>
2021-04-17 14:50:31 +0300

    Init dev push-client

--- bot/config/__init__.py
+++ bot/config/__init__.py
@@ -3,12 +3,14 @@ from pytz import timezone
 import logging
 from datetime import datetime
 
-from .config import DELAY, VK, RSS, IMAGE_PATH, RSS_FEEDS, VK_CHANNELS, STATUS
+from .config import DELAY, VK, RSS, IMAGE_PATH, RSS_FEEDS, VK_CHANNELS, STATUS, YOUTUBE
 from .database import events, videos, warns, notes, pidors, pidorstats, conn
+from .server import app, web
 from .logger import logging
 from .vk_api import vk_api
 from .bot import bot, dp
 
 START_TIME = datetime.now()
 TIMEZONE = timezone("Europe/Moscow")
-SCHEDULE = RSS or VK
+SCHEDULE = RSS or VK or YOUTUBE
+runner = web.AppRunner(app)
