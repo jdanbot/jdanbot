0c145f55340d4d8eac615d4adb7b2240622ea417
jDan735 <daniel734@bk.ru>
2021-04-23 01:48:29 +0300

    Update donate card

--- bot/memes/donate.py
+++ bot/memes/donate.py
@@ -2,7 +2,6 @@ from ..config import dp
 
 @dp.message_handler(commands=["donate"])
 async def donate(message):
-    await message.reply("<b>Визит очка (через umoney ака Я.Деньги):</b>"
-                        "\n\nhttps://yoomoney.ru/to/41001911960333",
+    await message.reply("<b>🇷🇺 UMoney aka Я.Деньги:</b> 5599 0050 8875 2808",
                         parse_mode="HTML",
                         disable_web_page_preview=True)
