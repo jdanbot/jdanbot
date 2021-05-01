9e9ef97ab2cd48a7348d55cf49dd58033df08783
jDan735 <daniel734@bk.ru>
2021-05-01 13:04:55 +0300

    Create more tasks for rss

--- bot.py
+++ bot.py
@@ -4,8 +4,9 @@ import aioschedule
 from aiogram import executor
 
 from bot import *  # noqa
-from bot.config import dp, polls, DELAY, RSS, VK, SCHEDULE, KATZ_BOTS
-from bot.timer import rss_timer
+from bot.config import (
+    dp, polls, DELAY, RSS, VK, SCHEDULE, KATZ_BOTS, RSS_FEEDS)
+from bot.timer import rss_task
 from bot.vk import vk_timer
 
 
@@ -15,9 +16,17 @@ asyncio.set_event_loop(loop)
 
 async def scheduler():
     if RSS:
-        aioschedule.every(DELAY).seconds.do(rss_timer)
+        for feed in RSS_FEEDS:
+            aioschedule.every(DELAY).seconds.do(
+                rss_task,
+                feed["url"],
+                feed["channelid"],
+                feed["chatid"]
+            )
+
     if VK:
         aioschedule.every(DELAY).seconds.do(vk_timer)
+
     if KATZ_BOTS:
         aioschedule.every(DELAY).seconds.do(polls.close_old)
 
