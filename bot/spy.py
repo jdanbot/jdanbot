9f0d8fd50cbca26b2bdfbdba7f9ac7c01e8db8e1
jDan735 <daniel734@bk.ru>
2021-05-13 01:49:09 +0300

    i18n in spy

--- bot/spy.py
+++ bot/spy.py
@@ -1,32 +1,24 @@
 from sqlfocus import SQLTable
 
-from .config import bot, dp, conn
-from .locale import locale
+from .config import bot, dp, conn, events, _
 from .lib.text import code, prettyword
 
 
 def _user_counter(users):
-    return prettyword(len(users), locale.users)
+    return prettyword(len(users), _("cases.users"))
 
 
 @dp.message_handler(commands=["me"])
 async def me_info(message):
-    msg_template = (
-        "*Full name:* {name}\n" +
-        "*ID:* `{id}`\n\n" +
-        "*Common chats*: `{chats}`\n" +
-        "*User status*: {status}"
-    )
-
-    table = SQLTable("events", conn)
-    chats = await table.select(where=[f"id={message.from_user.id}"])
+    chats = await events.select(where=events.id == message.from_user.id)
 
     user = await bot.get_chat_member(
         message.chat.id,
         message.from_user.id
     )
 
-    await message.reply(msg_template.format(
+    await message.reply(_(
+        "spy.about_user",
         name=message.from_user.full_name,
         id=message.from_user.id,
         chats=len(chats),
@@ -37,38 +29,31 @@ async def me_info(message):
 @dp.message_handler(lambda message: message.from_user.id == 795449748,
                     commands=["users"])
 async def calc_users(message):
+    chat_users = await events.select(where=events.chatid == message.chat.id)
+    chats_users = await events.select()
+
     users = []
-    table = SQLTable("events", conn)
+    users = [user[1] for user in chats_users if user[1] not in users]
 
-    chat_users = await table.select(where=[f"chatid={message.chat.id}"])
-    e = await table.select()
+    await message.reply(_(
+        "spy.users_info", 
+        local_users=len(chat_users),
+        lu_label=_user_counter(chat_users),
 
-    for user in e:
-        if user[1] in users:
-            pass
-        else:
-            users.append(user[1])
-
-    # В этом чяте <num> пользователей
-    # Всего <num> пользователей
-
-    text = "В этом чяте {} {}\nВсего {} {}"
-
-    await message.reply(text.format(len(chat_users), _user_counter(chat_users),
-                                    len(users), _user_counter(users)))
+        global_users=len(users),
+        gu_label=_user_counter(users)
+    ))
 
 
 async def activate_spy(message):
-    table = SQLTable("events", conn)
     user = message.from_user
-
-    events = await table.select(where=[
-        f"id={message.from_user.id}",
-        f"chatid={message.chat.id}"
+    cur_user = await events.select(where=[
+        events.id == user.id,
+        events.chatid == message.chat.id
     ])
 
-    if len(events) == 0:
-        await table.insert(message.chat.id, user.id, user.full_name)
+    if len(cur_users) == 0:
+        await events.insert(message.chat.id, user.id, user.full_name)
         await conn.commit()
     else:
         pass
