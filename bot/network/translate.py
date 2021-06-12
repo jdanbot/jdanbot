9fee29f7da57bbb3da50c7fddd65ac37f6d3b255
jDan735 <daniel734@bk.ru>
2021-06-12 14:34:12 +0300

    Use handlers in translate.py

--- bot/network/translate.py
+++ bot/network/translate.py
@@ -7,65 +7,46 @@ from random import choice
 t = Translator()
 
 
-async def getTranslate(message, lang):
-    opt = message.text.split(maxsplit=1)
-
-    if len(opt) != 1:
-        text = opt[1]
-    elif message.reply_to_message is not None:
-        if message.reply_to_message.text is None:
-            text = message.reply_to_message.caption
-
-        elif message.reply_to_message.text is not None:
-            try:
-                text = message.reply_to_message.text
-            except Exception as e:
-                await message.reply(_("errors.error"))
-                await bot.send_message("795449748", e)
-                return
-    else:
-        await message.reply(_("errors.reply_message"))
-        return
-
-    try:
-        translated = t.translate(text, dest=lang).text
-        if len(translated) >= 4096:
-            translated = translated[:4096]
-    except Exception as e:
-        await bot.send_message("795449748", e)
-        translated = e
-
-    await message.reply(translated)
-
-
 @dp.message_handler(commands=["tru"])
-async def tru(message):
-    await getTranslate(message, "ru")
+@handlers.get_text
+async def tru(message, text):
+    await message.reply(t.translate(text, dest="ru").text[:4096],
+                        disable_web_page_preview=True)
 
 
 @dp.message_handler(commands=["ten"])
-async def ten(message):
-    await getTranslate(message, "en")
+@handlers.get_text
+async def tru(message, text):
+    await message.reply(t.translate(text, dest="en").text[:4096],
+                        disable_web_page_preview=True)
 
 
 @dp.message_handler(commands=["tuk", "tua"])
-async def tua(message):
-    await getTranslate(message, "uk")
+@handlers.get_text
+async def tru(message, text):
+    await message.reply(t.translate(text, dest="ua").text[:4096],
+                        disable_web_page_preview=True)
 
 
 @dp.message_handler(commands=["tbe", "tby"])
-async def tbe(message):
-    await getTranslate(message, "be")
+@handlers.get_text
+async def tru(message, text):
+    await message.reply(t.translate(text, dest="be").text[:4096],
+                        disable_web_page_preview=True)
 
 
 @dp.message_handler(commands=["tpl"])
-async def tpl(message):
-    await getTranslate(message, "pl")
+@handlers.get_text
+async def tru(message, text):
+    await message.reply(t.translate(text, dest="pl").text[:4096],
+                        disable_web_page_preview=True)
 
 
 @dp.message_handler(commands=["tde"])
-async def tde(message):
-    await getTranslate(message, "de")
+@handlers.get_text
+async def tru(message, text):
+    await message.reply(t.translate(text, dest="de").text[:4096],
+                        disable_web_page_preview=True)
 
 
 @dp.message_handler(commands=["crazy"])
