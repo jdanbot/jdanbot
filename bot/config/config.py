b3bb3d1dcf57d89bed390a5b91b43b45de7cd148
jDan735 <daniel734@bk.ru>
2021-06-17 21:33:29 +0300

    Add france lang

--- bot/config/config.py
+++ bot/config/config.py
@@ -79,7 +79,7 @@ Config(
         "net_xua": "CAACAgIAAx0CUDyGjwACAQ9fCFkfgfI9pH9Hr96q7dH0biVjEwAC4gADlJlpL_foG56vPzRPGgQ"},
 
     token=None,
-    gtranslate_langs=["ru", "en", "uk", "be", "pl", "de"]
+    gtranslate_langs=["ru", "en", "ua", "uk", "be", "pl", "de", "fr"]
 )
 
 BASE_DIR = Path(__file__).parent.parent
