3e00a715aadbe620097fe353737fcae72342868e
jDan735 <daniel734@bk.ru>
2021-07-28 05:40:52 +0300

    fix of logic

--- bot/triggers/block.py
+++ bot/triggers/block.py
@@ -5,11 +5,11 @@ from aiogram.types import ContentType
 
 @dp.message_handler(lambda x: x.forward_from_chat and
 
-                              (x.from_user.id in (675257916,) and
-                               x.forward_from_chat.id in (-1001113237212,)) or
+                              ((x.from_user.id in (675257916,) and
+                                x.forward_from_chat.id in (-1001113237212,)) or
 
-                              (x.forward_from_chat.id in (-1001204336102,) and
-                               x.chat.id in (-1001176998310, -1001410092459)),
+                               (x.forward_from_chat.id in (-1001204336102,) and
+                                x.chat.id in (-1001176998310, -1001410092459))),
                     content_types=ContentType.ANY)
 async def unknown_message(message):
     await message.delete()
