0ba8db103e01822f628e8b890679b47f810754a2
TupaBan-Dev <78302942+TupaBan-Dev@users.noreply.github.com>
2021-08-01 01:22:43 +0300

    Ping Detailed & Fix mistakes X5
     Ping Detailed & Fix mistakes X5

--- bot/dev/test.py
+++ bot/dev/test.py
@@ -7,7 +7,7 @@ from ..lib.text import bold, code
 @dp.message_handler(commands=["ping", "p"])
 async def ping(message):
     start = perf_counter()
-    msg = await message.answer("Think...")
+    msg = await message.answer("⚾️ Think...")
     end = perf_counter()
-    await msg.edit_text(bold("Pong ") + code(round(end - start, 3)),
+    await msg.edit_text(bold("🏓 Pong ") + code(round(end - start, 3)) + code("s"),
                         parse_mode="HTML")
