5f420227fcfd7e9c7e0fe695eaa2771d1e3e8c10
Daniel Zakharov <daniel734@bk.ru>
2021-08-10 22:53:03 +0300

    Fix arrows in /mono

--- bot/network/mono.py
+++ bot/network/mono.py
@@ -17,26 +17,23 @@ def get_emoji(code):
 
 
 @dp.message_handler(commands=["mono"])
-async def navalny(message):
+async def monobank(message):
     res = await aioget("https://api.monobank.ua/bank/currency")
 
     msg = ""
     js = res.json()
 
-    if isinstance(js, dict) and js["errorDescription"] is not None:
+    if isinstance(js, dict) and js["errorDescription"]:
         await message.reply(f"<code>{js['errorDescription']}</code>",
                             parse_mode="HTML")
         return
    
-    for info in js[::2]:
-        try:
-            msg += '{} ⇆ {}\n← {} ₴\n→ {} ₴\n\n'.format(
-                    get_emoji(info["currencyCodeA"]),
-                    get_emoji(info["currencyCodeB"]),
-                    info["rateBuy"],
-                    info["rateSell"]
-                )
-        except:
-            pass
+    for info in js[::2][:3]:
+        msg += '{} ⇆ {}\n→ {} ₴\n← {} ₴\n\n'.format(
+            get_emoji(info["currencyCodeA"]),
+            get_emoji(info["currencyCodeB"]),
+            info.get("rateBuy"),
+            info.get("rateSell")
+        )
 
     await message.reply(msg)
