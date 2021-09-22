f87116f5623bd6bf55808585f8cc288afb149e10
jdan735 <daniel734@bk.ru>
2021-09-22 20:07:48 +0300

    New error output

--- bot/triggers/error.py
+++ bot/triggers/error.py
@@ -1,11 +1,21 @@
 import traceback
+import io
 
 from ..config import dp, bot, _, LOGGING_CHAT
-from ..lib.text import bold, code
+from ..lib.text import fixHTML, bold, code
+
+
+log_schema = """
+{name} #{id}
+<b>query:</b> {query}
+
+#{error_small}
+"""
 
 
 @dp.errors_handler()
 async def catch_error(callback, exception):
+    message = callback.message
     error = traceback.format_exc()
     inf_err = error.split("\n")[-2]
 
@@ -18,12 +28,29 @@ async def catch_error(callback, exception):
         return
 
     if LOGGING_CHAT is not None:
-        await bot.send_message(LOGGING_CHAT, code(error),
-                               parse_mode="HTML")
+        f = io.StringIO(error)
+        f.name = f"{exc}.log"
 
-    if callback.message is None:
+        if message.chat.id < 0:
+            id = str(message.chat.id).replace("-100", "chat")
+        else:
+            id = f"user{message.chat.id}"
+
+        await bot.send_document(
+            LOGGING_CHAT, document=f,
+            caption=log_schema.format(
+                name=bold(getattr(message.chat, "first_name") or
+                          message.chat.title),
+                query=fixHTML(message.text),
+                error_small=inf_err,
+                id=id
+            ),
+            parse_mode="HTML"
+        )
+
+    if message is None:
         return
 
-    await callback.message.reply(bold(_("errors.error")) + "\n"
-                                 + code(inf_err),
-                                 parse_mode="HTML")
+    await message.reply(bold(_("errors.error")) + "\n"
+                             + code(inf_err),
+                             parse_mode="HTML")
