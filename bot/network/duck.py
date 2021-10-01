3ad7344777216b36daf3eba184e824223f120e5d
Daniel Zakharov <daniel734@bk.ru>
2021-10-01 12:34:56 +0300

    Update depends

--- bot/network/duck.py
+++ bot/network/duck.py
@@ -13,13 +13,11 @@ async def getDuck(message, params):
     query = params[1]
     text = ""
 
-    links = await duck.HTML_search(query)
+    links = await duck.search(query)
 
     for link in links[:10]:
-        url = "https:{url}".format(url=link["url"])
         link["title"] = bold(fixWords(link["title"]))
-        title = "<a href='{url}'>{title}</a>".format(title=link["title"],
-                                                     url=url)
+        title = f"<a href='{link['url']}'>{link['title']}</a>"
 
         text += title + "\n"
         text += f"{link['description']}\n\n"
