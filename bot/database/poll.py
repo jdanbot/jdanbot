5904a0ebd9635b1cb56b9bad2eba09a681b6e55b
Daniel Zakharov <daniel734@bk.ru>
2022-01-23 13:21:54 +0300

    Refactor poll code

--- bot/database/poll.py
+++ bot/database/poll.py
@@ -1,13 +1,11 @@
+from aiogram import types
 from datetime import datetime, timedelta
 
 from .connection import db
+from .chat_member import ChatMember
 from ..config.bot import bot
 
-from .chat_member import ChatMember
-from .chat import Chat
-
 from peewee import CharField, DateTimeField, ForeignKeyField, Model
-from aiogram.utils import exceptions
 
 
 class Poll(Model):
@@ -19,27 +17,12 @@ class Poll(Model):
         db_table = "polls"
         database = db
 
-    async def close(self):
+    async def close(self) -> types.Poll:
+        Poll.delete().where(Poll.id == self.id).execute()
         return await bot.stop_poll(self.author.chat.id, self.id)
 
-    async def close_old():
-        period = datetime.now() - timedelta(hours=24)
-        polls = Poll.select().where(Poll.created_at <= period).execute()
+    async def close_old(period: timedelta = timedelta(hours=24)):
+        polls = Poll.select().where(Poll.created_at <= datetime.now() - period)
 
         for poll in polls:
-            try:
-                poll_res = await poll.close()
-
-            except (exceptions.PollHasAlreadyBeenClosed,
-                    exceptions.MessageWithPollNotFound):
-                (Poll.delete()
-                     .where(Poll.created_at <= period,
-                            Poll.id == poll.id).execute())
-
-                continue
-
-            if poll_res.is_closed:
-                Poll.delete().where(Poll.created_at <= period).execute()
-
-            else:
-                await bot.stop_poll(poll.chat_id, poll.id)
+            await poll.close()
