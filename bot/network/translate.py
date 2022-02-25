066bdb052b0b61ba20e885fb01e0c9395e53723d
Daniel Zakharov <daniel734@bk.ru>
2022-02-25 20:48:54 +0300

    Extra translator patch

--- bot/network/translate.py
+++ bot/network/translate.py
@@ -1,5 +1,7 @@
 from aiogram import types
 
+from deep_translator import GoogleTranslator as DeepGoogleTranslator
+
 from ..lib.multitran import GoogleTranslator
 from ..config import dp, GTRANSLATE_LANGS
 from ..lib import handlers
@@ -11,19 +13,17 @@ from random import choice
 @dp.message_handler(commands=[f"t{lang}" for lang in GTRANSLATE_LANGS])
 @handlers.get_text
 async def translate(message: types.Message, query: str):
-    t = GoogleTranslator()
-
     if (command := message.get_command().split("@")[0]) != "/tua":
         lang = command[2:]
     else:
         lang = "uk"
 
-    text = await t.translate(query, tgt_lang=lang)
+    t = DeepGoogleTranslator(source="auto", target=lang)
+
+    text = t.translate(query, tgt_lang=lang)
 
     await message.reply(cute_crop(text, limit=4096),
                         disable_web_page_preview=True)
-
-    await t.close()
 
 
 @dp.message_handler(commands=["crazy"])
