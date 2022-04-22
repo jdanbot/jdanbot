40ca6f2d1dcf14a12419584f5ffc0460b9be15bf
Daniel Zakharov <daniel734@bk.ru>
2022-04-23 02:11:20 +0300

    Fix memepedia

--- bot/network/memepedia.py
+++ bot/network/memepedia.py
@@ -6,6 +6,8 @@ from ..config import dp, _
 from .. import handlers
 from ..lib.models import Article
 
+from tghtml import TgHTML
+
 
 @dp.message_handler(commands=["memepedia", "meme"])
 @handlers.send_article
@@ -17,9 +19,18 @@ async def mempep(message: types.Message, query: str) -> Article:
         await message.reply(_("errors.not_found"))
         return
 
-    page = await pymemeru.page(search[0]["name"])
+    page = await pymemeru.page(search[0].name)
+    text = TgHTML(str(page.cleared_text)).parsed.replace('\n', '\n\n')
+
+    text2 = f"""
+<b>{page.title}</b>
+<i>{page.published_at}</i> 👀 {page.views}\n
+{text}
+"""
 
     return Article(
-        text=page[1],
-        image=page[0]
+        text=text2,
+        image=page.main_image,
+        test=True,
+        href=f"https://memepedia.ru/{search[0].name}"
     )
