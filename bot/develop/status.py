9bd114624ec27edbe07644f4b7533d27d6f02581
Daniel Zakharov <gzdan734@gmail.com>
2022-06-19 09:21:20 +0300

    Update /status style, fix #139

--- bot/develop/status.py
+++ bot/develop/status.py
@@ -1,10 +1,13 @@
 from aiogram import types
 
 import subprocess  # noqa: S404
-from datetime import datetime
+from datetime import datetime, timedelta
 from sys import platform
 
+import humanize
+
 import psutil
+import distro
 import toml
 
 from ..config import dp, settings, START_TIME, _
@@ -17,27 +20,40 @@ with open("pyproject.toml", "r") as f:
 __version__ = pyproject["tool"]["poetry"]["version"]
 
 
+def pprint_timedelta(timedelta: timedelta) -> str:
+    s = timedelta.total_seconds()
+
+    days,    remainder = divmod(s,         60*60*24)
+    hours,   remainder = divmod(remainder, 60*60)
+    minutes, seconds   = divmod(remainder, 60)
+
+    return "{:02}:{:02}:{:02}:{:02}".format(
+        int(days),
+        int(hours),
+        int(minutes),
+        int(seconds)
+    )
+
+
 @dp.message_handler(commands=["status"])
 async def get_status(message: types.Message):
     mem = psutil.virtual_memory()
-
     time = datetime.now() - START_TIME
 
     await message.reply(_(
         "dev.status",
         name=settings.status,
         branch=get_current_branch(),
-        platform=platform,
+        platform=distro.id() if platform == "linux" else platform,
         version=__version__,
 
-        memory=convert_bytes(mem.used),
-        total_memory=convert_bytes(mem.total),
-
-        cpu=psutil.cpu_percent(),
-        uptime=str(time).split(".")[0]
+        memory=humanize.naturalsize(mem.used, binary=True),
+        total_memory=humanize.naturalsize(mem.total, binary=True),
+        uptime=pprint_timedelta(time)
     ), parse_mode="Markdown")
 
 
 def get_current_branch() -> str:
     git_command = "git rev-parse --abbrev-ref HEAD"
+
     return subprocess.check_output(git_command.split()).decode("utf-8").strip()
