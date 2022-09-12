9a4adb92c161be9054b5c168de8038b7983982db
Daniel Zakharov <gzdan734@gmail.com>
2022-09-13 01:28:37 +0300

    add twitter redirect

---
+++ bot/triggers/xztwitter.py
@@ -0,0 +1,15 @@
+from ..config import dp
+from aiogram import types
+from .. import handlers
+import urllib.parse
+
+
+@dp.message_handler(lambda x: "https://twitter.com/" in x.text.lower())
+@handlers.check("enable_twitter_redirect")
+async def replace_twitter(message: types.Message):
+    for entitie in message.entities:
+        raw_url = message.text.lower()[entitie.offset:entitie.offset + entitie.length]
+
+        if entitie.type in {"url"} and "https://twitter.com/" in raw_url:
+            url = urllib.parse.urlparse(raw_url)
+            return await message.reply(url._replace(netloc="zxtwitter.com").geturl())
