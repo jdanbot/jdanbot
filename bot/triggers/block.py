94263d58cb8cfa876b52c5a42f7a35f00346fbfd
Daniel Zakharov <gzdan734@gmail.com>
2022-11-16 12:00:26 +0300

    update block.py

--- bot/triggers/block.py
+++ bot/triggers/block.py
@@ -5,18 +5,18 @@ from aiogram import types
 
 @dp.message_handler(
     lambda x: x.forward_from_chat
-    and x.chat.id in (-1001176998310,)
+    and x.chat.id in [-1001176998310]
     and (
         (
-            x.from_user.id in (675257916, 811510365)
-            and x.forward_from_chat.id in (-1001113237212,)
+            x.from_user.id in [675257916, 811510365]
+            and x.forward_from_chat.id in [-1001113237212]
         )
-        or (x.forward_from_chat.id in (-1001204336102,))
+        or (x.forward_from_chat.id in [-1001204336102])
         or (
-            x.from_user.id in (
+            x.from_user.id in [
                 # 1890967276,
-                679350651,
-            )
+                # 679350651,
+            ]
             and x.forward_from_chat.id < 0
         )
     ),
