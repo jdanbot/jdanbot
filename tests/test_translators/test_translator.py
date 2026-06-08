374dde6b5a332faa323f78ee5599b01e86067766
Daniel Zakharov <gzdan734@gmail.com>
2026-06-08 17:15:56 +0300

    skip old unrewritten tests

--- tests/test_translators/test_translator.py
+++ tests/test_translators/test_translator.py
@@ -4,6 +4,7 @@ from bot.translator.translator import translate
 from tests.mocks import mock
 
 
+@pytest.mark.skip("rewrite")
 @pytest.mark.asyncio
 async def test_translate_handler():
     assert (
