a77b9ab07264096fc557f0b890a747e6a2c111c7
Daniel Zakharov <gzdan734@gmail.com>
2026-06-20 00:42:37 +0300

    Create init_tables.sql

---
+++ queries/init_tables.sql
@@ -0,0 +1,68 @@
+-- name: init_tables#
+CREATE TABLE if not exists "chat" (
+	"id"	INTEGER NOT NULL,
+	"title"	TEXT NOT NULL,
+	"username"	TEXT,
+	"language"	TEXT,
+	"pidor_id"	INT,
+	"settings"	JSONB,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE if not exists "command" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "chat_id" INT NOT NULL,
+    "user_id" INT NOT NULL,
+    "name" TEXT NOT NULL,
+    "args" TEXT NOT NULL
+);
+
+CREATE TABLE if not exists "note" (
+	"id"	INTEGER NOT NULL,
+	"chat_id"	INT NOT NULL,
+	"name"	TEXT NOT NULL,
+	"text"	TEXT NOT NULL,
+	"author_id"	INT NOT NULL,
+	"created_at"	INT DEFAULT CURRENT_TIMESTAMP,
+	"editor_id"	INT,
+	"updated_at"	INT,
+	"is_locked" BOOLEAN DEFAULT 0,
+	UNIQUE("chat_id","name"),
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE if not exists "pidor" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "chat_id" INT NOT NULL,
+    "is_allowed" BOOLEAN NOT NULL DEFAULT 1,
+    "latest_time" INT,
+    "user_id" BIGINT NOT NULL,
+	UNIQUE("chat_id","user_id")
+);
+
+CREATE TABLE if not exists "pidorevent" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "chat_id" INT NOT NULL,
+    "caused_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "pidor_id" INT NOT NULL
+);
+
+CREATE TABLE if not exists "user" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT,
+    "username" TEXT
+);
+
+CREATE TABLE if not exists "warns" (
+	"id"	INTEGER NOT NULL,
+	"chat_id"	INTEGER NOT NULL,
+	"victim_id"	INTEGER NOT NULL,
+	"warn_admin_id"	INTEGER NOT NULL,
+	"reason"	VARCHAR(255) NOT NULL,
+	"warned_at"	DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	"unwarn_admin_id"	INTEGER,
+	"unwarn_reason"	VARCHAR(255),
+	"unwarned_at"	DATETIME,
+	PRIMARY KEY("id")
+);
