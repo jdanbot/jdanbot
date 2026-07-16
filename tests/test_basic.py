93423f44429689f25d8faf8669a1581e0fa7cf59
Daniel Zakharov <gzdan734@gmail.com>
2026-07-17 01:52:38 +0300

    update art test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,5 +1,5 @@
 from aiogram.utils.markdown import code
-from art import text2art
+from stripped import text2art
 
 from bot.config.lib.locales import locales
 
@@ -52,7 +52,7 @@ class Tests(AbcTests):
         await self.assertBulkMock(
             (
                 "/art test",
-                code(text2art("test", chr_ignore=True)),
+                code(text2art("test", True)),
             ),
             ("/calc 5+5", "`10`"),
             # ("/eval 2*2+2", "`6`"),
